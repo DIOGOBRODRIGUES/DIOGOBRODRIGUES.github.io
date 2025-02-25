0655f06da96fa52dc4ac8b6200e21a834f8803ae
DIOGO F B RODRIGUES <diogo.borbar@gmail.com>
2025-02-24 23:20:23 -0300

    adiciona novo projeto React com estrutura inicial e configurações básicas

---
+++ pages/programacaoMobile/aula3/exemplos/formReact/meu-projeto/src/components/Formulario.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+
+const Formulario: React.FC = () =>{
+    const fields = {
+        {label:"Nome", id:"noome", type="text", placeholder:"Digite seu nome"},
+        
+    }
+}
