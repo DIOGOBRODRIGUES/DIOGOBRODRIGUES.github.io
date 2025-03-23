eeb64209a3d0f1c1474729f088009bbe3395c1fe
DIOGO F B RODRIGUES <diogo.borbar@gmail.com>
2025-03-23 14:43:52 -0300

    aula 6 - mobile e remocao de lixo aula 3

--- pages/programacaoMobile/aula3/exemplos/formReact/meu-projeto/src/components/Formulario.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-
-const Formulario: React.FC = () =>{
-    const fields = {
-        {label:"Nome", id:"noome", type="text", placeholder:"Digite seu nome"},
-        
-    }
-}
